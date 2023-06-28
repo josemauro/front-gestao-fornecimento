3d2e23af7c5e8229353ffe611363f31ab509785f
Jose Mauro <z.mauro@live.com>
2023-06-28 02:54:15 -0300

    Integra botão de deletar

--- src/components/FornecedoresBusca.jsx
+++ src/components/FornecedoresBusca.jsx
@@ -1,45 +1,93 @@
+import axios from "axios";
 import { useState } from "react";
 import ResultadoBusca from "./ResultadoBusca";
 
 function FornecedoresBusca() {
     // eslint-disable-next-line no-unused-vars
-    const [resultados, setResultados] = useState([{id:"123", nome: "jose"}]);
+    const [resultados, setResultados] = useState([]);
+    const [filtro, setFiltro] = useState("nome");
+    const [valor, setValor] = useState("");
 
+
+    const handleSubmit = async e => {
+
+        e.preventDefault();
+        
+        try {
+            await axios.get("http://localhost:8181/fornecedores/listar/" + filtro + "?valor=" + valor)
+                        .then((response) => {
+
+                                                if (response.status == 200){
+                                                    
+                                                    if (response.data == null) {
+                                                        setResultados([]);
+                                                        return;
+                                                    }
+                                                    
+                                                    if (filtro == 'nome'){
+                                                        setResultados(response.data);    
+                                                    } else {
+                                                        setResultados([response.data]);
+                                                    }
+                                                    
+                                                } else {
+                                                throw response.data;
+                                                }
+                                            })
+        } catch (err) {
+            if (err.response.status == 404){
+                alert("Nenhum fornecedor encontrado!");
+            } else alert("Erro ao buscar fornecedor!");
+        }
+        
+      };
 
     return (
         <div className="container-fluid px-4">
             <h1 className="mt-4">Buscar um fornecedor</h1>
             <div className="card mt-5">
                 <div className="card-body">
+                <form  onSubmit={handleSubmit}>
                     <div style={{width: "350px", marginLeft: "200px", marginRight: "200px", textAlign: "left"}}>    
-
-                    <label className="fs-4">
-                        Selecione um filtro de busca:
-                    </label>
-               
-                    <div className="form-check mt-3 fs-5">
-                        <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1"/>
-                        <label className="form-check-label" htmlFor="flexRadioDefault1">
-                            Nome
+                        <label className="fs-4">
+                            Selecione um filtro de busca:
                         </label>
-                    </div>
+                
+                        <div className="form-check mt-3 fs-5">
+                            <input className="form-check-input" type="radio" name="flexRadioDefault"
+                                   value="nome" 
+                                   id="flexRadioDefault1"
+                                   onChange={e => setFiltro(e.target.value)}
+                                   checked/>
+                            <label className="form-check-label" htmlFor="flexRadioDefault1">
+                                Nome
+                            </label>
+                        </div>
 
-                    <div className="form-check fs-5">
-                        <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2"/>
-                        <label className="form-check-label" htmlFor="flexRadioDefault2">
-                            Identificador (CPF/CNPJ)
-                        </label>
+                        <div className="form-check fs-5">
+                            <input className="form-check-input" type="radio" name="flexRadioDefault"
+                                   value="id" 
+                                   onChange={e => setFiltro(e.target.value)}
+                                   id="flexRadioDefault2"/>
+                            <label className="form-check-label" htmlFor="flexRadioDefault2">
+                                Identificador (CPF/CNPJ)
+                            </label>
+                        </div>
                     </div>
-                </div>
-                <form className="form-inline my-2 my-lg-0" style={{marginLeft: "200px", marginRight: "200px"}}>
-                    <input className="form-control mt-4 mb-5 mr-sm-2" type="search" placeholder="Busca" aria-label="Search"/>
-                    <button className="btn btn-primary btn-lg" type="submit">Buscar</button>
-                    </form>
+                    <div className="form-inline my-2 my-lg-0" style={{marginLeft: "200px", marginRight: "200px"}}>
+                        <input className="form-control mt-4 mb-5 mr-sm-2" type="search"
+                               onChange={e => setValor(e.target.value)}
+                               placeholder="Busca"
+                               aria-label="Search"
+                               required/>
+                        <button className="btn btn-primary btn-lg" type="submit">Buscar</button>
+                    </div>
+                </form>
                 </div>
             </div>
 
             <ul className="list-group my-4">
-                {resultados ? resultados.map(fornecedor => (<ResultadoBusca key={fornecedor.id} nome={fornecedor.nome} />)) : ""}
+                {resultados ? resultados.map(fornecedor => (<ResultadoBusca key={fornecedor.id} nome={fornecedor.nome} id={fornecedor.id} />)) : ""}
             </ul>
             
         </div>        
--- src/components/FornecedoresCadastro.jsx
+++ src/components/FornecedoresCadastro.jsx
@@ -1,26 +1,67 @@
+import axios from "axios";
+import { useState } from "react";
 
 function FornecedoresCadastro() {
+    const [nome, setNome] = useState('');
+    const [id, setId] = useState('');
+    const [email, setEmail] = useState('');
+
+    const handleSubmit = async e => {
+
+        e.preventDefault();
+
+        
+        try {
+            await axios.post("http://localhost:8181/fornecedores/salvar?id="+id+"&nome="+nome+"&email="+email, )
+                        .then((response) => {
+
+                                                console.log("teste", response);
+                                                if (response.status == 200){
+                                                    alert("Salvo com sucesso!");
+                                                } else {
+                                                throw response.data;
+                                                }
+                                            })
+        } catch (err) {
+            if (err.response.status == 409){
+                alert("Erro ao salvar, fornecedor já existe!");
+            } else alert("Erro ao cadastrar fornecedor!");
+        }
+        
+      };
 
     return (
         <div className="container-fluid px-4 fs-5">
             <h1 className="mt-4">Cadastro de fornecedores</h1>
             <div className="card mt-5">
                 <div className="card-body">
-                <form style={{marginLeft: "200px", marginRight: "200px"}}>
-                    <div className="form-group mt-4 mb-4">
-                        <label htmlFor="formGroupExampleInput">Nome</label>
-                        <input type="text" className="form-control mt-4" id="formGroupExampleInput" placeholder="Nome completo"/>
-                    </div>
-                    <div className="form-group mt-4 mb-4">
-                        <label htmlFor="formGroupExampleInput2">Identificador</label>
-                        <input type="text" className="form-control mt-4" id="formGroupExampleInput2" placeholder="CNPJ/CPF"/>
-                    </div>
-                    <div className="form-group mt-4 mb-4">
-                        <label htmlFor="formGroupExampleInput3">E-mail</label>
-                        <input type="text" className="form-control mt-4" id="formGroupExampleInput3" placeholder="e-mail"/>
-                    </div>
-                    <button type="submit" className="btn btn-primary btn-lg">Cadastrar</button>
-                </form>
+                    <form style={{marginLeft: "200px", marginRight: "200px"}} onSubmit={handleSubmit}>
+                        <div className="form-group mt-4 mb-4">
+                            <label className="form-label">Nome</label>
+                            <input type="text" className="form-control mt-4"
+                                   placeholder="Nome completo"
+                                   onChange={e => setNome(e.target.value)}
+                                   required/>
+                            <div className="invalid-feedback">
+                                Por favor informe um nome.
+                            </div>
+                        </div>
+                        <div className="form-group mt-4 mb-4">
+                            <label>Identificador</label>
+                            <input type="text" className="form-control mt-4"
+                                   placeholder="CNPJ/CPF"
+                                   onChange={e => setId(e.target.value)}
+                                   required/>
+                        </div>
+                        <div className="form-group mt-4 mb-4">
+                            <label>E-mail</label>
+                            <input type="text" className="form-control mt-4"
+                                   placeholder="e-mail"
+                                   onChange={e => setEmail(e.target.value)}
+                                   required/>
+                        </div>
+                        <button type="submit" className="btn btn-primary btn-lg">Cadastrar</button>
+                    </form>
                 </div>
             </div>
         </div>        
