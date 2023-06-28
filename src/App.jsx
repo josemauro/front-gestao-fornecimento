4f7196a8352e77b5468a34c6def6c7ca37a7b682
Jose Mauro <z.mauro@live.com>
2023-06-27 22:35:42 -0300

    Add suporte as rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
-import SistemaGerenciamentoFornecimento from './components/SistemaGerenciamentoFornecimento'
+// import SistemaGerenciamentoFornecimento from './components/SistemaGerenciamentoFornecimento'
+import Rotas from './routes/Rotas'
 
 function App() {
 
   return (
     <>
-      <SistemaGerenciamentoFornecimento/>
+      <Rotas/>
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossOrigin="anonymous"></script>
     </>
   )
